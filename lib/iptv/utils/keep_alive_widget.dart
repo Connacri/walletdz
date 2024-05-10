16a33a92472382d17993aefe0abfdb4a2949b39d
Connacri <95408708+Connacri@users.noreply.github.com>
2024-05-10 14:34:18 +0100

    iptv

---
+++ lib/iptv/utils/keep_alive_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../about.dart';
+
+class KeepAliveWidget extends StatefulWidget {
+  final VideoPlayerScreen parent;
+
+  const KeepAliveWidget({required this.parent});
+
+  @override
+  _KeepAliveWidgetState createState() => _KeepAliveWidgetState();
+}
+
+class _KeepAliveWidgetState extends State<KeepAliveWidget>
+    with AutomaticKeepAliveClientMixin<KeepAliveWidget> {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.parent;
+  }
+}
